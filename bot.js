2ff77c1a7f804067df1b90ec35ec9ac35fc6faa4
Michael Wong <michwong98@gmail.com>
2020-02-07 16:11:44 -0500

    Wrapper class for MySQL. Added more reaction commands.

--- bot.js
+++ bot.js
@@ -4,6 +4,43 @@ var mysql = require("mysql")
 
 var client = new Discord.Client();
 
+//Wrapper class for MySQL client.
+class Database {
+
+	constructor(auth) {
+		 this.connection = mysql.createConnection({
+			host: auth.db_host,
+			user: auth.db_user,
+			password: auth.db_pw,
+			database: auth.db_name
+		});
+
+		//Database connect.
+		this.connection.connect(function (error) {
+			if (error) {
+				throw error;
+			}
+			console.log("Database connected.");
+
+			//Discord Bot Client connect.
+			client.login(auth.token)
+			.catch(console.error);
+		});
+	} //End constructor.
+
+	//Query function.
+	query(sql, args) {
+		return new Promise( (resolve, reject) => {
+			this.connection.query(sql, args, (error, result) => {
+				if (error) return reject(error);
+				resolve(result);
+			});
+		});
+	}
+}
+
+const database = new Database(auth);
+
 client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	});
@@ -45,31 +82,29 @@ client.on('message', async msg => {
 				const newMsg = await msg.channel.send(`> **Event Name**: *${msgName}*\n> **Event Time**: *${msgTime}*\n> **Capacity**: *0/${msgCapacity}*\n> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
 				msg.delete();
 				//Reaction commands to interact with scheduled event.
-				await newMsg.react("✅"); await newMsg.react("❌");
+				await newMsg.react("✅"); await newMsg.react("❌"); await newMsg.react("🚫");
 
-				//Creates table to save event members.
-				db.query(`CREATE TABLE message_${newMsg.id} (user_id VARCHAR(20), username TINYTEXT);`, function(error, result) {
-					if (error) {
-						throw error;
-					}
-
-					//Creates new message object.
-
-					//Record for new event message.
-					db.query(`INSERT INTO messages (channel_id, message_id, message_name, message_time, capacity) VALUES ('${newMsg.channel.id}', '${newMsg.id}', '${msgName}', '${msgTime}', '${msgCapacity}');`, function(error, result) {
-						if (error) {
-							throw error;
-						}
-						console.log(`${newMsg.id}, New Event Created: ${msgName}`);
-					});
-
-				});
+				//Create table for new event.
+				database.query(`CREATE TABLE message_${newMsg.id} (user_id VARCHAR(20) NOT NULL PRIMARY KEY, username TINYTEXT NOT NULL);`)
+				.then(() => {
+					//Create record for event message info.
+					return database.query(`INSERT INTO messages (channel_id, message_id, message_name, message_time, capacity) VALUES ('${newMsg.channel.id}', '${newMsg.id}', '${msgName}', '${msgTime}', '${msgCapacity}');`);
+				})
+				.then(() => {
+					console.log(`${newMsg.id}: New Event Created, "${msgName}"`);
+				})
+				.catch(console.error)
 
 				break;
 
 			case "info":
 			case "help":
-				msg.channel.send("Use **NewEvent** to schedule a new event!\n```e!newevent [Event Name (default: No Name Given)], [Event Time (default: No Time Given)], [Maximum Capacity (default: 10)]```");
+				msg.channel.send("Use **NewEvent** to schedule a new event!\n"
+					+ "```e!newevent [Event Name (default: No Name Given)], [Event Time (default: No Time Given)], [Maximum Capacity (default: 10)]\n"
+					+ "Example: \ne!newevent Example Gathering, Friday 16:00, 15```\n"
+					+ "Users react with :white_check_mark: and :x: to join and leave events.\n"
+					+ ":no_entry_sign: deletes the event (permissions required)."
+					);
 				break;
 		}
 
@@ -78,100 +113,136 @@ client.on('message', async msg => {
 
 client.on("raw", async packet => {
 	if (packet.t === "MESSAGE_REACTION_ADD" && packet.d.user_id !== auth.user_id) {
+		let message_info;
+		database.query(`SELECT * FROM messages where message_id = '${packet.d.message_id}';`)
+		.then(rows => {
+			if (rows.length <= 1)  {
+				//Message exists in database.
+				message_info = rows[0];
+				const databasePromise = database.query(`SELECT * FROM message_${packet.d.message_id};`);
+				const userPromise = client.fetchUser(packet.d.user_id);
+				const messagePromise = client.channels.get(message_info.channel_id).fetchMessage(message_info.message_id);
+
+				Promise.all([databasePromise, userPromise, messagePromise]).then(values => {
+					const eventRoster = values[0];
+					const userObj = values[1];
+					const messageObj = values[2];
+
+					//Removes the added reaction.
+					messageObj.reactions.forEach(reaction => reaction.remove(packet.d.user_id));
+
+					switch (packet.d.emoji.name) {
+						case "✅": //Adds member to event roster.
+							if (eventRoster.length < message_info.capacity && eventRoster.reduce((memberObj, member) => member.user_id === packet.d.user_id ? member : memberObj, null) === null) {
+								database.query(`INSERT INTO message_${message_info.message_id} (user_id, username) VALUES ('${userObj.id}','${userObj.username}');`);
+
+								//Converts event roster to string.
+								var rosterString = eventRoster.reduce((rosterString, member) =>  rosterString + `> *${member.username}*\n`, "");
+								rosterString += `> *${userObj.username}*\n`;
+
+								//Updates message.
+								messageObj.edit(`> **Event Name:**  *${message_info.message_name}*\n` +
+										`> **Event Time:**  *${message_info.message_time}*\n` +
+										`> **Capacity:** *${eventRoster.length + 1}/${message_info.capacity}*\n` +
+										`> \n${rosterString}` +
+										`> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
+							}
+						break;
+					case "❌": //Removes member from event roster.
+						var indexDelete;
+						if ((indexDlete = eventRoster.reduce((memberIndex, member, index) => member.user_id === packet.d.user_id ? index : memberIndex, null)) !== null) {
+							//Deletes record from message table.
+							database.query(`DELETE FROM message_${message_info.message_id} WHERE user_id = '${userObj.id}'`);
+
+							//Removes user object from array.
+							eventRoster.splice(indexDelete, 1);
+
+							//Converts event roster to string.
+							var rosterString = eventRoster.reduce((rosterString, member) =>  rosterString + `> *${member.username}*\n`, "");
+
+							//Updates message.
+							messageObj.edit(`> **Event Name:**  *${message_info.message_name}*\n` +
+										`> **Event Time:**  *${message_info.message_time}*\n` +
+										`> **Capacity:** *${eventRoster.length + 1}/${message_info.capacity}*\n` +
+										`> \n${rosterString}` +
+										`> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
+						}
+						break;
+					case "🚫": //Removes event. Checks for permissions.
+						//User can manage messages.
+						if (messageObj.channel.permissionsFor(userObj).has("MANAGE_MESSAGES", false)) {
+							database.query(`DROP TABLE message_${message_info.message_id}`);
+							database.query(`DELETE FROM messages WHERE message_id = ${message_info.message_id}`);
+							messageObj.delete()
+							.then(msg => console.log(`${message_info.message_id}: Event deleted.`));
+						}
+						break;
+				} //End switch statement.
+			});
+		}
+		})
+		.catch(console.error);
+	}
+});
+
+/*
+client.on("raw", async packet => {
+	if (packet.t === "MESSAGE_REACTION_ADD" && packet.d.user_id !== auth.user_id) {
 		try {
 
-			db.query(`SELECT * FROM messages WHERE message_id = '${packet.d.message_id}';`, function (error, messages) {
-				if (error) {
-					throw error;
-				}
+				let message_info, message_roster, user_infom, eventRoster;
+				database.query(`SELECT * FROM messages where message_id = '${packet.d.message_id}';`)
+				.then((message_record) => {
+					if (message_record.length !== 0) {
+						message_info = message_record[0];
+						return packet.d.emoji.name;
+					}
+					return new Error(`${packet.d.message_id}: Message not recorded.`)
+				})
+				.then((emoji) => {
+					switch (emoji) {
+						case "✅":
+						console.log(message_info);
+							database.query(`SELECT * FROM message_${message_info.message_id}`)
+							.then(rows => {
+								eventRoster = rows;
+								if (eventRoster.length < message_info.capacity) {
+									let rosterMember = eventRoster.reduce((memberObj, member) => member.user_id === packet.d.user_id ? member : memberObj, null);
+									if (rosterMember === null) {
+										message_roster = eventRoster;
+										return client.fetchUser(packet.d.user_id);
+									}
+								}
+								throw new Error(`${packet.d.message_id}: Failed to add new member to roster.`);
+							})
+							.then(user => {
+								user_info = {id: user.id, username: user.username};
+								return database.query(`INSERT INTO message_${message_info.message_id} (user_id, username) VALUES ('${user.id}','${user.username}');`);
+							})
+							.then(result => {
+								return client.channels.get(message_info.channel_id).fetchMessage(message_info.message_id);
+							})
+							.then(msg => {
+								msg.reactions.forEach(reaction => reaction.remove(packet.d.user_id));
+								var rosterString = message_roster.reduce((rosterString, member) =>  rosterString + `> *${member.username}*\n`, "");
+								rosterString += `> *${user_info.username}*\n`;
 
-				if (messages.length !== 0) {
-					
-					var message_info = messages[0];
-
-					client.channels.get(message_info.channel_id).fetchMessage(message_info.message_id)
-					.then(msg => {
-						//Removes added reaction.
-						msg.reactions.forEach(reaction => reaction.remove(packet.d.user_id));
-
-						if (packet.d.emoji.name === "✅" || packet.d.emoji.name === "❌") {
-							db.query(`SELECT * FROM message_${message_info.message_id};`, function (error, eventRoster) {
-
-								if (error) throw error;
-
-								switch (packet.d.emoji.name) {
-
-									case "✅":
-
-										//Check capacity.
-										if (eventRoster.length < message_info.capacity) {
-
-											//Is the user a member of the event roster?
-											let memberObj = eventRoster.reduce((memberObj, member) => member.user_id === packet.d.user_id ? member : memberObj, null);
-
-											//User is not a member of the event roster.
-											if (memberObj === null) {
-												client.fetchUser(packet.d.user_id)
-												.then(user => {
-
-													//Insert new member into event roster.
-													db.query(`INSERT INTO message_${message_info.message_id} (user_id, username) VALUES ('${user.id}','${user.username}');`, function(error, result) {
-														if (error) throw error;
-
-														//Update message.
-														var rosterString = eventRoster.reduce((rosterString, member) =>  rosterString + `> *${member.username}*\n`, "");
-														rosterString += `> *${user.username}*\n`;
-
-														msg.edit(`> **Event Name:**  *${message_info.message_name}*\n` +
-															`> **Event Time:**  *${message_info.message_time}*\n` +
-															`> **Capacity:** *${eventRoster.length + 1}/${message_info.capacity}*\n` +
-															`> \n${rosterString}` +
-															`> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
-													});
-
-												});
-
-											} //End if memberObj.
-
-										} //End if capacity.
-
-										break;
-
-								} //End switch statement.
-								
-							});
-
-						}
-
-					});
-
-				} //End message.length if.
-
-			});
+								msg.edit(`> **Event Name:**  *${message_info.message_name}*\n` +
+										`> **Event Time:**  *${message_info.message_time}*\n` +
+										`> **Capacity:** *${eventRoster.length + 1}/${message_info.capacity}*\n` +
+										`> \n${rosterString}` +
+										`> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
+							})
+							.catch();
+						break;
+					}
+				})
+				.catch();
 
 		}
 		catch (error) {
-			console.error(error);
 		}
 		
 	}
 });
-
-var db = mysql.createConnection({
-	host: auth.db_host,
-	user: auth.db_user,
-	password: auth.db_pw,
-	database: auth.db_name
-});
-
-//Database connect.
-db.connect(function (error) {
-	if (error) {
-		throw error;
-	}
-	console.log("Database connected.");
-
-	//Discord Bot Client connect.
-	client.login(auth.token)
-	.catch(console.error);
-});
+*/
