74a55b96bc7a4dfcbab617e3dcb479ac606e6216
Michael Wong <michwong98@gmail.com>
2020-02-14 20:54:47 -0500

    Command handler.

--- bot.js
+++ bot.js
@@ -48,75 +48,20 @@ client.on('ready', () => {
 	console.log(`Logged in as ${client.user.tag}!`);
 	});
 
-client.on('message', async msg => {
-	if (msg.content.substring(0, 2) == "e!") {
-		var cmd = msg.content.substring(2).split(" ")[0].toLowerCase();
-		var x = msg.content.indexOf(" ");
-		var args = (x === -1 ? "" : msg.content.substring(x + 1));
-		switch (cmd) {
-			case "newevent":
-				args = args.split(",");
-
-				try {
-					//Validates input.
-					var msgName = (/\S+/.test(args[0].trim()) ? args[0].trim() : "No Name Given");
-				}
-				catch (error) {
-					//Default name for scheduled event.
-					var msgName = "No Name Given";
-				}
-
-				try {
-					var msgTime = args[1].trim();
-				}
-				catch (error) {
-					//Default value for scheduled time.
-					var msgTime = "No Time Given";
-				}
-				try {
-					//Validates unsigned number.
-					var msgCapacity = (/^\d+$/.test(args[2].trim()) ? args[2].trim() : 10);
-				} catch (error) {
-					//Default value of 10 for maximum capacity.
-					//TODO: Default no capacity.
-					var msgCapacity = 10;
-				}
-
-				const newMsg = await msg.channel.send(`> **Event Name**: *${msgName}*\n> **Event Time**: *${msgTime}*\n> **Capacity**: *0/${msgCapacity}*\n> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
-				newMsg.pin();
-				msg.delete();
-				//Reaction commands to interact with scheduled event.
-				await newMsg.react("✅"); await newMsg.react("❌"); await newMsg.react("🚫");
-
-				//Create table for new event.
-				database.query(`CREATE TABLE message_${newMsg.id} (user_id VARCHAR(20) NOT NULL PRIMARY KEY, username TINYTEXT NOT NULL);`)
-				.then(() => {
-					//Create record for event message info.
-					return database.query(`INSERT INTO messages (channel_id, message_id, message_name, message_time, capacity) VALUES ('${newMsg.channel.id}', '${newMsg.id}', '${msgName}', '${msgTime}', '${msgCapacity}');`);
-				})
-				.then(() => {
-					console.log(`${newMsg.id}: New Event Created, "${msgName}"`);
-				})
-				.catch(console.error)
-
+client.on("raw", async packet => {
+	//Don't run on unrelated packets.
+	if (["MESSAGE_REACTION_ADD", "MESSAGE_CREATE"].includes(packet.t) && packet.d.user_id !== auth.user_id) {
+		switch(packet.t) {
+			case "MESSAGE_REACTION_ADD":
+				handler.onReaction(packet);
 				break;
-
-			case "info":
-			case "help":
-				msg.channel.send("Use **NewEvent** to schedule a new event!\n"
-					+ "```e!newevent [Event Name (default: No Name Given)], [Event Time (default: No Time Given)], [Maximum Capacity (default: 10)]\n"
-					+ "Example: \ne!newevent Example Gathering, Friday 16:00, 15```\n"
-					+ "Users react with :white_check_mark: and :x: to join and leave events.\n"
-					+ ":no_entry_sign: deletes the event (permissions required)."
-					);
+			case "MESSAGE_CREATE":
+				if (packet.d.content.substring(0, 2) === "e!") {
+					handler.onCommand(packet);
+				}
+				break;
+			default:
 				break;
 		}
-
-	}
-});
-
-client.on("raw", async packet => {
-	if (packet.t === "MESSAGE_REACTION_ADD" && packet.d.user_id !== auth.user_id) {
-		handler.onReaction(packet);
 	}
 });
--- handler.js
+++ handler.js
@@ -1,3 +1,5 @@
+//TODO: CHECK MESSAGE CACHE BEFORE FETCH.
+
 class Handler {
 
 	//Constructor.
@@ -48,6 +50,24 @@ class Handler {
 		.catch(console.error);
 	} //End onReaction function.
 
+	async onCommand(packet) {
+		let command = packet.d.content.substring(2).split(" ");
+		//Validate command isn't an empty string.
+		if (command.length === 0 || (command = command[0].trim().toLowerCase()) === "") {
+			return;
+		}
+		switch (command) {
+			case "info":
+			case "help":
+				this.commandHelp(packet); //Displays help info.
+				break;
+			case "newevent":
+				this.commandNew(packet); //Create new event message.
+				break;
+		}
+
+	}
+
 	addMember(args) {
 		if (args.eventRoster.length < args.message_info.capacity && args.eventRoster.reduce((memberObj, member) => member.user_id === args.packet.d.user_id ? member : memberObj, null) === null) {
 			//Creates record of user.
@@ -58,11 +78,11 @@ class Handler {
 			rosterString += `> *${args.userObj.username}*\n`;
 
 			//Updates message.
-			args.messageObj.edit(`> **Event Name:**  *${args.message_info.message_name}*\n` +
+			args.messageObj.edit(`@here \n> **Event Name:**  *${args.message_info.message_name}*\n` +
 					`> **Event Time:**  *${args.message_info.message_time}*\n` +
 					`> **Capacity:** *${args.eventRoster.length + 1}/${args.message_info.capacity}*\n` +
 					`> \n${rosterString}` +
-					`> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
+					`> \n> *React with :white_check_mark: to join.*`);
 		} //End if.
 	} //End addMember function.
 
@@ -83,7 +103,7 @@ class Handler {
 						`> **Event Time:**  *${args.message_info.message_time}*\n` +
 						`> **Capacity:** *${args.eventRoster.length}/${args.message_info.capacity}*\n` +
 						`> \n${rosterString}` +
-						`> \n> *Please be gentle with me. I am hosted on a potato.*\n> *React with :white_check_mark: to join.*`);
+						`> \n> *React with :white_check_mark: to join.*`);
 		} //End if.
 	} //End removeMember function.
 	
@@ -93,9 +113,56 @@ class Handler {
 			this.database.query(`DROP TABLE message_${args.message_info.message_id}`);
 			this.database.query(`DELETE FROM messages WHERE message_id = ${args.message_info.message_id}`);
 			args.messageObj.delete()
-			.then(msg => console.log(`${args.message_info.message_id}: Event deleted.`));
+			.then(msg => console.log(`${args.message_info.message_id}: Event deleted.`))
+			.catch(console.error);
 		}
 	} //End deleteEvent function.
+
+	async commandNew(packet) { //Create new event.
+		var args = packet.d.content.substring(2).split(",");
+		//Parse args.
+		args = this.getArgsNewEvent(...args);
+
+		const channel = this.client.channels.get(packet.d.channel_id);
+
+		//Send new event message.
+		const newMessage = await channel.send(`@here \n> **Event Name**: *${args[0]}*\n> **Event Time**: *${args[1]}*\n> **Capacity**: *0/${args[2]}*\n> \n> *React with :white_check_mark: to join.*`);
+		newMessage.pin();
+		await newMessage.react("✅"); await newMessage.react("❌"); await newMessage.react("🚫");
+
+		//Deletes command message.
+		channel.fetchMessage(packet.d.id).then(message => message.delete());
+
+		//Create table for new event.
+		this.database.query(`CREATE TABLE message_${newMessage.id} (user_id VARCHAR(20) NOT NULL PRIMARY KEY, username TINYTEXT NOT NULL);`)
+		.then(() => {
+			//Create record for event message info.
+			return this.database.query(`INSERT INTO messages (channel_id, message_id, message_name, message_time, capacity) VALUES ('${channel.id}', '${newMessage.id}', '${args[0]}', '${args[1]}', '${args[2]}');`);
+		})
+		.then(() => {
+			console.log(`${newMessage.id}: New Eevent Created, "${args[0]}"`);
+		})
+		.catch(console.error);
+		return;
+	} //End commandNew function.
+
+	getArgsNewEvent(name = "No Name Given", time = "No Time Given", capacity = "10") {
+		name = name.trim() === "" ? "No Name Given" : name.trim();
+		time = time.trim() === "" ? "No Time Given" : time.trim();
+		capacity = /^\d+$/.test(capacity.trim()) ? capacity.trim() : 10;
+		return [name, time, capacity];
+	} //end getArgsNewEvent.
+
+	async commandHelp(packet) {
+		this.client.channels.get(packet.d.channel_id).send(
+			"Use **NewEvent** to schedule a new event!\n"
+			+ "```e!newevent [Event Name (default: No Name Given)], [Event Time (default: No Time Given)], [Maximum Capacity (default: 10)]\n"
+			+ "Example: \ne!newevent Example Gathering, Friday 16:00, 15```\n"
+			+ "Users react with :white_check_mark: and :x: to join and leave events.\n"
+			+ ":no_entry_sign: deletes the event (permissions required)."
+			);
+		return;
+	} //End commandHelp function.
 
 }
 
